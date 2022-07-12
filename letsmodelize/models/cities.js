702d6fcd372e673db84f86d5926f0be70e2f6fc2
Matthieu Petruzzi <matthieu.petruzzi@gmail.com>
2022-07-12 10:54:57 +0200

    add letsmodelize

---
+++ letsmodelize/models/cities.js
@@ -0,0 +1,11 @@
+// Insert your code here
+const mongoose = require('mongoose')
+const citySchema = mongoose.Schema({
+  name: String,
+  currentPopulation: Number,
+  country: { type: mongoose.Schema.Types.ObjectId, ref: 'countries' },
+})
+
+const City = mongoose.model('cities', citySchema)
+
+module.exports = City
---
+++ letsmodelize/models/countries.js
@@ -0,0 +1,18 @@
+// Insert your code here
+const mongoose = require('mongoose')
+
+const populationSchema = mongoose.Schema({
+  populationNbr: Number,
+  year: Date,
+})
+const countrySchema = mongoose.Schema({
+  name: String,
+  flagImg: String,
+  currency: String,
+  population: [populationSchema],
+})
+
+const Country = mongoose.model('countries', countrySchema)
+
+module.exports = Country
+//
